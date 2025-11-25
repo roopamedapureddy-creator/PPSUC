2aff4fa0290dc028b1ec739ae1d49bffacda1110
roopa@123 <roopamedapureddy@gmail.com>
2025-11-25 12:02:07 +0530

    Add files via upload

---
+++ v_c_d_s.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	char a[30];
+	int i,v=0,c=0,d=0,s=0;
+	scanf("%[^\n]s", a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+		v++;
+		else if(a[i]==' ')
+		s++;
+		else if(a[i]>='0'&&a[i]<='9')
+		d++;
+		else
+		c++;
+	}
+	printf("vowels are:%d\n",v);
+	printf("characters are:%d\n",s);
+	printf("digits are:%d\n",d);
+	printf("consonants are:%d\n",c);
+}
