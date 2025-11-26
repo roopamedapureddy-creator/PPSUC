ad732e62c6c0b9459a555e153b5a10c9554cfa5e
roopa@123 <roopamedapureddy@gmail.com>
2025-11-26 21:23:51 +0530

    Add files via upload

---
+++ contest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int X,A,B;
+    scanf("%d%d%d",&X,&A,&B);
+    int total = A*1+B*2;
+    if(total>=X)
+    printf("Qualify");
+    else
+    printf("Not Qualify");
+    return 0;
+}
